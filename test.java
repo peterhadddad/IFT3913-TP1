61161d8ef5bad1108906f4888d6dd3769843042f
Bassel Assi <bassel_assi@hotmail.com>
2023-09-28 16:02:45 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,21 +1,7 @@
-package com.example.tp13913;
+package com.example.tp1;
 
-import static org.testng.AssertJUnit.assertEquals;
-
-public class test {
-    public static void main(String[] args) {
-        int result = add(5, 7);
-        int a=1;
-        int b=1;
-
-        // Assert that the result is as expected
-        assert result == 12 : "Error: The result is not as expected!";
-
-
-        System.out.println("The result is: " + result);
-    }
-
-    public static int add(int a, int b) {
-        return a + b;
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hi");
     }
 }
