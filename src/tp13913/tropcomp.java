d942feff87250e9b554c380bf3be9f05331a23d8
Bassel Assi <bassel_assi@hotmail.com>
2023-10-05 13:26:50 -0400

    tropcomp.java & numero 5
    - tropcomp fixed and working
    - added numero 5

--- src/tp13913/tropcomp.java
+++ src/tp13913/tropcomp.java
@@ -1,34 +1,72 @@
 package tp13913;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.*;
 
-public class tropcomp {
 
-    public static void main(String[] args) {
-        if (args.length < 2 || args.length>4 || args[0].equals("-o") ) {
+public class tropcomp {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length < 2) {
             System.out.println("Usage: java tropcomp <chemin du dossier> <seuil>%");
             return;
         }
-        String entree=args[args.length-2];
-        String sortie= args.length==4?args[1]:null;
-        double seuil =Double.parseDouble(args[args.length-1]);
-        List<String> lignesSortie = new ArrayList<>();
-        try {
-            FileWriter writer = new FileWriter("output.csv");
-            for (String line : lignesSortie) {
-                writer.write(line + "\n");
-            }
-            writer.close();
-            System.out.println("Resultat en output.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String outputFile = args[1];
+        String cheminDentree = args[2];
+        double  seuil = Double.valueOf(args[3]);
 
+        List<String> ligneSortie = new ArrayList<>();
+        ligneSortie = processFolder(new File(cheminDentree), ligneSortie);
+        try{
+            if ("-o".equals(args[0])) {
+                outputFile = args[1];
+                args = Arrays.copyOfRange(args, 2, args.length);
+                printConsole(ligneSortie,seuil);
+            }
+        }
+        catch(NumberFormatException e){
+            System.out.println("Valide");
+        }
 
     }
 
+    public static List<String> processFolder(File folder,List<String> ligneCSV) throws FileNotFoundException {
 
+        if (folder.isDirectory()) {
+            for (File file : folder.listFiles()) {
+                List<String> Array = new ArrayList<>();
+                if (file.isDirectory()) {
+                    ligneCSV.addAll(processFolder(file, Array));
+                } else if (file.getName().endsWith(".java")) {
+                    ligneCSV.addAll(tls.processJavaFile(file, Array));
+                    continue;
+                }
+            }
+            isOrganized(ligneCSV);
+        }
+        return ligneCSV;
+    }
+
+    public static void isOrganized(List<String> allClasses) {
+        allClasses.sort(Comparator.comparing(testClass -> {
+            String[] metrics = testClass.split(",");
+            double tpcmDeClass = Double.parseDouble(metrics[5]);
+            return tpcmDeClass;
+        }));
+
+    }
+    public static int pourcentageSeuil(double seuil,int longeur){
+        double valeur;
+        if(longeur==0){
+            throw new IllegalArgumentException("Pas faisable");
+        }
+        valeur=(seuil*longeur)/100;
+        return (int) valeur;
+    }
+    private static void printConsole(List<String> ligneSortie,double seuil) {
+        int valeur=pourcentageSeuil(seuil,ligneSortie.size());
+        for(int i=0;i<=valeur;i++){
+            System.out.println(ligneSortie.get(i));
+        }
+    }
 }
